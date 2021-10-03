d54496abae85659af8f39acd24bdb8e6d737e3f9
HP <HP@LAPTOP-A6B6VJG4>
2021-10-03 23:08:03 +0530

    hiiiii

---
+++ SDET/src/OOP_Employee.java
@@ -0,0 +1,20 @@
+
+public class OOP_Employee {
+
+int id;
+String name;
+char block;
+int salary;
+int batchno;
+
+ void getdata()
+ {
+	 System.out.println(id);
+	 System.out.println(name);
+	 System.out.println(block);
+	 System.out.println(salary);
+	 System.out.println(batchno);  
+	 System.out.println(id+"  "+name+"  "+block +"  "+salary+"  "+batchno);
+ }
+}
+
---
+++ SDET/src/OOP_employye_1.java
@@ -0,0 +1,15 @@
+
+public class OOP_employye_1 {
+public static void main(String[] args) {
+	
+	OOP_Employee obj = new OOP_Employee();
+	obj.id=15;
+	obj.name="rohit";
+	obj.block='a';
+	obj.batchno=57;
+	obj.salary=15000;
+	
+	obj.getdata();
+	
+}	
+}
